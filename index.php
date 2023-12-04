17e66e37f2734ca0e1da79167ca7c2cbe79f04e3
Finnedorn <daniele.giuntoli@outlook.com>
2023-12-04 15:09:13 +0100

    added basic scaffholding

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+include __DIR__."/views/header.php";
+//setto che movie possa essere incluso in index altriemnti non vedrò mai cosa stampo
+include __DIR__."/model/Movie.php";
+?>
+    <main>
+        <div class="container">
+            <h2>
+                Movies
+            </h2>
+        </div>
+    </main>
+<?php 
+include __DIR__."/views/footer.php";
+?>
---
+++ views/footer.php
@@ -0,0 +1,6 @@
+
+    <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
